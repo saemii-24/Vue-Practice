1b6f645618b2b82bcd54c823d4ea83a1b91fd385
saemii-24 <hims2419@gmail.com>
2024-06-12 21:32:12 +0900

    Feat: click 이벤트 추가

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
+    <button @:click="increaseLike(i)">좋아요</button>
+    <span>{{ movie.like }}</span>
   </div>
 </template>
 
@@ -19,19 +21,27 @@ export default {
           title: "노량",
           year: 2023,
           category: "액션, 드라마",
+          like: 0,
         },
         {
           title: "아쿠아맨과 로스트 킹덤",
           year: 2023,
           category: "액션, 판타지, 어드벤처",
+          like: 0,
         },
         {
           title: "3일의 휴가",
           year: 2023,
           category: "판타지, 드라마",
+          like: 0,
         },
       ],
     };
+  },
+  methods: {
+    increaseLike(i) {
+      this.data[i].like += 1;
+    },
   },
 };
 </script>
