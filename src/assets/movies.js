63058b8184322a1f6479e953dd9a5d3102dce8e4
saemii-24 <hims2419@gmail.com>
2024-06-13 16:50:57 +0900

    Feat: data js로 분리하기

---
+++ src/assets/movies.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    title: "어른제국의 역습",
+    year: 2001,
+    category: "액션, 드라마",
+    like: 0,
+    imgUrl: "assets/어른제국의역습.webp",
+  },
+  {
+    title: "격돌! 낙서왕국과 얼추 네 명의 용사들",
+    year: 2020,
+    category: "액션, 판타지, 어드벤처",
+    like: 0,
+    imgUrl: "assets/낙서왕국.webp",
+  },
+  {
+    title: "수수께끼! 꽃피는 천하떡잎학교",
+    year: 2021,
+    category: "판타지, 드라마",
+    like: 0,
+    imgUrl: "assets/천하떡잎학교.webp",
+  },
+];
+
+export default data;
